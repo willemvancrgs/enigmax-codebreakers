a3ee3baa46acd9f50eee4c434c873d5eddc7c55a
AuburnMischief <frazer.commit@gmail.com>
2025-10-30 17:20:16 +0000

    Refactored Morse code program

--- cryptotools/Morse_Code.py
+++ cryptotools/Morse_Code.py
@@ -1,67 +1,84 @@
-#Made by Tudor Ciobotaru
+"""
+Founded by Tudor Ciobotaru...
+Improved by Frazer
+"""
 
-def solve(ciphertext):
-    decode(ciphertext)
+text_to_morse = {
+    "A": ".-", "B": "-...", "C": "-.-.", "D": "-..", "E": ".", "F": "..-.",
+    "G": "--.", "H": "....", "I": "..", "J": ".---", "K": "-.-", "L": ".-..",
+    "M": "--", "N": "-.", "O": "---", "P": ".--.", "Q": "--.-", "R": ".-.",
+    "S": "...", "T": "-", "U": "..-", "V": "...-", "W": ".--", "X": "-..-",
+    "Y": "-.--", "Z": "--..",
+    
+    "0": "-----", "1": ".----", "2": "..---", "3": "...--", "4": "....-",
+    "5": ".....", "6": "-....", "7": "--...", "8": "---..", "9": "----.",
+    
+    "<HH>": "........", "&": ".-...", "'": ".----.", "@": ".--.-.", ")": "-.--.-",
+    "(": "-.--.", ":": "---...", ",": "--..--", "=": "-...-", "!": "-.-.--",
+    ".": ".-.-.-", "-": "-....-", "*": "-..-", "+": ".-.-.", "\\": ".-..-.",
+    "?": "..--..", "/": "-..-."
+}
 
-dictionary = [".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-", ".--","-..-","-.--","--..","-----",".----","..---","...--","....-",".....","-....","--...","---..","----."]
-Alphabet_and_numbers = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z","0","1","2","3","4","5","6","7","8","9"]
-punctuation_morse = ["........",".-...",".----.",".--.-.","-.--.-","-.--.","---...","--..--","-...-","-.-.--",".-.-.-","-....-","-..-",".-.-.",".-..-.","..--..","-..-."]
-punctuation = ["<HH>", "&", "'", "@", ")", "(", ":", ",", "=", "!", ".", "-", "*", "+", "\\", "?", "/"]
-decoded = False
-encoded = False
-def decode(morse):
-    encodedWords = morse.split("/")
-    encodedSentence = []
-    for each in encodedWords:
-        encodedSentence.append(each.split(" "))
-    decodedSentence = []
-    for word in encodedSentence:
+morse_to_text = {v: k for k, v in text_to_morse.items()}
+
+def split(morse: str, word_gap: str = " /") -> list[list[str]]:
+    """Split the morse into letters grouped into words."""
+    split_sentence = [word.split(" ") for word in morse.split(word_gap)]
+    return split_sentence
+    
+def decode(morse: str, word_gap: str = " /"):
+    """Decode the morse."""
+    split_sentence = split(morse)
+    decoded_sentence = ""
+    
+    for word in split_sentence:
         for letter in word:
-            if letter in dictionary:
-                decodedSentence.append(Alphabet_and_numbers[dictionary.index(letter)])
-            elif letter in punctuation_morse:
-                decodedSentence.append(punctuation[punctuation_morse.index(letter)])
-        decodedSentence.append(" ")
-    decodedSentenceOriginal = "".join(decodedSentence)
-    decodedSentenceVer2 = decodedSentenceOriginal.replace("0/0","%")
-    #decoded = True
-    #print(decodedSentenceOriginal)
-    return (decodedSentenceVer2)
+            if letter in morse_to_text:
+                decoded_sentence += morse_to_text[letter]
+        decoded_sentence += " "
+    
+    decoded_sentence = decoded_sentence.replace("0/0", "%")
+    
+    return decoded_sentence
  
-def encode(english):
-    if "%" in english:
-        english.replace("%","0/0")
-    finalSentence = []
-    originalSentence = english.upper()
-    originalSentence = originalSentence.split(" ")
-    for word in originalSentence:
-        letters = [char for char in word.strip()]
-        finalSentence.append(letters)
-    encodedSentence = []
-    for word in finalSentence:
-        for letter in word:
-            if letter in Alphabet_and_numbers:
-                encodedSentence.append(dictionary[Alphabet_and_numbers.index(letter)])
-            elif letter in punctuation_morse:
-                encodedSentence.append(punctuation_morse[punctuation.index(letter)])
-            encodedSentence.append(" ")
-        encodedSentence.append("/")
-    return ("".join(encodedSentence))
+def encode(text):
+    """Encode text to morse."""
+    text = text.replace("%","0/0")
+    
+    text = text.upper()
+    split_text = text.split(" ")
+    
+    morse = ""
+    for word in split_text:
+        morse += " ".join([text_to_morse[letter] for letter in word]) + " / "
+    
+    return morse[:-3]
+
+def cli() -> None:
+    """Provide basic CLI for decoding and encoding in morse code."""
+    escape = False
+    while not escape:
+        choice = input("Decode, encode or quit? D / E / Q: ").upper()
+        match choice:
+            case "D":
+                morse = input("Input morse code:\n")
+                print(decode(morse)+"\n")
+            case "E":
+                text = input("Input standard text:\n")
+                print(encode(text)+"\n")
+            case "Q":
+                escape = True
+            case _:
+                print("Invalid input!\n")
 
 if __name__ == "__main__":
-    while decoded == False and encoded == False:
-        decodeOrEncode = input("Decode or Encode?\n").lower()
-        if decodeOrEncode == "decode":
-            decode(input())
-            Continue = input("Continue? (y/n)")
-            if Continue == "n" or Continue == "no":
-                decoded = True
-            elif Continue == "y" or Continue == "yes":
-                decoded = False
-        elif decodeOrEncode == "encode":
-            encode(input())
-            Continue = input("Continue? (y/n)")
-            if Continue == "n" or Continue == "no":
-                encoded = True        
-            elif Continue == "y" or Continue == "yes":
-                encoded = False
+    # Test Code
+    cipher = ".. / .-.. --- ...- . / - .... . / -. .- - .. --- -. .- .-.. / -.-. .. .--. .... . .-. / -.-. .... .- .-.. .-.. . -. --. . / ..--- ----- ..--- ..... -.-.--"
+    text = "the quick BROWN fox jumps over THE laZy dog!! 0123456789 %!.*"
+    print(split(cipher))
+    print(decode(cipher))
+    print(encode(text))
+    
+    # CLI
+    print("\n")
+    cli()
