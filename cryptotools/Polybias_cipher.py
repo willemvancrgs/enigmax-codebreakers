01af3305b086824274818f644fb1cfd987e74f5f
willemvancrgs <willemvan@crgs.co.uk>
2025-11-10 15:23:03 +0000

    Refactor polybias_decode and add main guard
    Refactor the Polybius cipher decoding function to improve readability and structure. Added a main guard for better script execution.

--- cryptotools/Polybias_cipher.py
+++ cryptotools/Polybias_cipher.py
@@ -1,4 +1,7 @@
-def polybius_decode(cipher_text):
+def polybias_decode(cipher_text):
+    """
+    Decodes the polybias cipher
+    """
     polybius_square = {"11": "A", "12": "B", "13": "C", "14": "D", "15": "E","21": "F", "22": "G", "23": "H", "24": "I/J", "25": "K","31": "L", "32": "M", "33": "N", "34": "O", "35": "P","41": "Q", "42": "R", "43": "S", "44": "T", "45": "U","51": "V", "52": "W", "53": "X", "54": "Y", "55": "Z"}
     cleaned = "".join(ch for ch in cipher_text if ch.isdigit())
     decoded_message = []
@@ -9,5 +12,9 @@ def polybius_decode(cipher_text):
     decoded = "".join(decoded_message)
     print("Decoded message:", decoded)
 
-cipher_text = input("Enter Polybius cipher text (digits, spaces allowed): ").strip()
-polybius_decode(cipher_text)
+solve = polybias_decode
+
+if __name__ == "__main__":
+    cipher_text = input("Enter Polybius cipher text (digits, spaces allowed): ").strip()
+    polybias_decode(cipher_text)
+
