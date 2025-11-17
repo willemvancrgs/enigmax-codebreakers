ff8b996c9fd9d0d8f526f895981d6a2661e364a9
willemvancrgs <willemvan@crgs.co.uk>
2025-11-17 15:39:13 +0000

    Make code more legible, move to main repo structure

---
+++ ciphers/baconian.py
@@ -0,0 +1,36 @@
+lookup = {
+"A":"AAAAA","B":"AAAAB",
+"C":"AAABA","D":"AAABB",
+"E":"AABAA","F":"AABAB",
+"G":"AABBA","H":"AABBB",
+"I":"ABAAA","J":"ABAAA","K":"ABAAB",
+"L":"ABABA","M":"ABABB",
+"N":"ABBAA","O":"ABBAB",
+"P":"ABBBA","Q":"ABBBB",
+"R":"BAAAA","S":"BAAAB",
+"T":"BAABA","U": "BAABB", "V": "BAABB",
+"W":"BABAA","X":"BABAB",
+"Y":"BABBA", "Z":"BABBB"
+}
+ 
+def decrypt_baconian_cipher(ciphertext):
+    """"""
+    decipher = ''
+    x = 0
+    while x < len(ciphertext):
+        if ciphertext[x] != ' ':
+            substr = ciphertext[x:x + 5] #makes a substring 5 letters long if there is acc smth in that index
+            keys_list = list(lookup.keys())
+            values_list = list(lookup.values())
+            decipher += keys_list[values_list.index(substr)] #finds which letter matches that 5 letter substring and adds it to decipher string
+            x += 5
+        else:
+            decipher += ' '
+            x += 1 #skip a space
+    print(decipher)
+
+solve = decrypt_baconian_cipher
+
+if __name__ == "__main__":
+    cipher = input("Cipher text: ").upper()
+    decrypt_baconian_cipher(cipher)
